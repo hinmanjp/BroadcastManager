1f5ba4e77bdf150715780d81e611038ae59f95d4
Peter Hinman <peter@hinmans.net>
2023-03-21 17:19:07 -0600

    Added embedded WebRTC stream viewer
    Former-commit-id: 816c8256b8dc022b4d032ac06458702db3d23d6d

---
+++ Components/StreamViewer.razor.js
@@ -0,0 +1,18 @@
+﻿export function startPlayer(stream_url) {
+
+    const player = OvenPlayer.create('player_id', {
+        "autoStart": true,
+        "mute": true,
+        "autoReload": true,
+        "autoReloadInterval": "2000",
+        sources: [
+            {
+                label: 'label_for_webrtc',
+                // Set the type to 'webrtc'
+                type: 'webrtc',
+                // Set the file to WebRTC Signaling URL with OvenMediaEngine 
+                file: stream_url
+            }
+        ]
+    });
+}
