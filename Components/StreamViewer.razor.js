bc75d2b39b45d911e37daa1157961663f841e030
Peter Hinman <peter@hinmans.net>
2023-03-25 14:01:31 -0600

    Make player size dynamic Better configure player look & controls
    Former-commit-id: 4b05ece238d7872d0df9d7a0f25f18a2ae25e16e

--- Components/StreamViewer.razor.js
+++ Components/StreamViewer.razor.js
@@ -1,16 +1,25 @@
 ﻿export function startPlayer(stream_url, stream_type) {
 
-    const player = OvenPlayer.create('player_id', {
-        "autoStart": true,
-        "mute": true,
-        "autoReload": true,
-        "autoReloadInterval": "2000",
-        sources: [
-            {
-                label: 'label_for_webrtc',
-                type: stream_type,
-                file: stream_url
-            }
-        ]
+    //let player = OvenPlayer.create("player", { autoStart: true, mute: false, disableSeekUI: true, timecode: false, volume: 0,  title: "Church broadcast live stream (offline)", controls: true, currentProtocolOnly: true, sources: [{ host: stream_url, application: 'app', stream: "stream", label: "WebRTC 1080P" }] });
+    let player = OvenPlayer.create("player", { autoStart: true, mute: false, disableSeekUI: true, timecode: false, volume: 0, title: "Church broadcast live stream (offline)", controls: true, currentProtocolOnly: true, sources: [{ file: stream_url, type: stream_type , label: "livestream" }] });
+
+    player.on('ready', function () {
+        player.getConfig().systemText.api.error[501].message = 'Waiting for live stream...';
     });
+
+
+
+    //const player = OvenPlayer.create('player_id', {
+    //    "autoStart": true,
+    //    "mute": true,
+    //    "autoReload": true,
+    //    "autoReloadInterval": "2000",
+    //    sources: [
+    //        {
+    //            label: 'label_for_webrtc',
+    //            type: stream_type,
+    //            file: stream_url
+    //        }
+    //    ]
+    //});
 }
