48834c8379aaa8cadaf80f57deaf04b8b3d6a08b
Peter Hinman <peter@hinmans.net>
2023-04-02 22:45:29 -0600

    Restore DNS registration, XForwardedFor header handling
    Former-commit-id: 0d7bc1347977a6fba614cc9eae46fbd196dc1537

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
 using BroadcastManager2;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
@@ -18,11 +19,26 @@ builder.Services.AddDataProtection()
 
 var app = builder.Build();
 
+AppSettings.Config = app.Configuration;
+
+// update dns records so that the broadcast manager can be found
+var dnsSplit = DnsHelper.SplitDnsName(app.Configuration["LocalServerDnsName"] ?? "");
+string ipv4Address = DnsHelper.GetLocalIPv4();
+
+var dns = new UpdateCloudflareDNS(app.Configuration["CloudFlareTokenKey"] ?? "");
+await dns.UpdateDnsAsync( dnsSplit.ZoneName, dnsSplit.RecordName, ipv4Address, new CancellationToken() );
+
+
 if ( !app.Environment.IsDevelopment() )
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseForwardedHeaders( new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+} );
 
 app.UseHttpsRedirection();
 
